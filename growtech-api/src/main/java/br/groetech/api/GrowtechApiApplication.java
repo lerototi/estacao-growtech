0df5f28a5d6f430897a560c3cb9a19e920d83099
Lerototi <lerototi@gmail.com>
2019-05-07 10:37:18 -0300

    initial commit api

---
+++ growtech-api/src/main/java/br/groetech/api/GrowtechApiApplication.java
@@ -0,0 +1,13 @@
+package br.groetech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowtechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowtechApiApplication.class, args);
+	}
+
+}
