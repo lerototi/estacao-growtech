404c8640aca38db909d79403ebad958d5c497c35
Lerototi <lerototi@gmail.com>
2019-05-07 10:36:47 -0300

    reorder project

---
+++ main/main.ino
@@ -0,0 +1,152 @@
+#include "DHT.h"
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
+
+#define DHTPIN 12
+#define DHTTYPE DHT11
+
+ESP8266WebServer server(80);
+
+DHT dht(FHTPIN, DHTTYPE);
+
+float t;
+float h;
+
+//output variables GPIO pins
+const int output2 = 2;
+
+//Variable to store the HTTP request
+String header;
+
+//Auxiliar variables to store the current state
+String output2State = "off";
+
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    //initialize the output variables as outputs
+    pinMode(output2, OUTPUT);
+
+    //Set outputs to Low
+    digitalWrite(output2,LOW);
+    
+
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+   
+    //reset saved settings
+    //wifiManager.resetSettings();
+    
+    //set custom ip for portal
+    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("AutoConnectAP");
+    
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+
+    
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+
+    server.begin();
+
+    
+}
+
+void loop() {
+
+
+   WiFiClient client = server.available(); //Listen for incoming clients
+
+
+  if(client) {
+    Serial.println("New Client");
+    String currentLine = "";
+    while(client.connected()) {
+      if(client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        header += c;
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            //turns the GPIOs on and off
+            if (header.indexOf("GET /2/on") >= 0) {
+              Serial.println("GPIO 2 on");
+              output2State = "on";
+              digitalWrite(output2, HIGH);
+            }else if (header.indexOf("GET /2/off") >= 0){
+              Serial.println("GPIO 2 off");
+              output2State = "off";
+              digitalWrite(output2, LOW);
+              }
+
+
+            //display the HTML web page
+
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+
+             // Web Page Heading
+            client.println("<body><h1>ESP8266 Web Server</h1>");
+
+             // Display current state, and ON/OFF buttons for GPIO 2  
+            client.println("<p>GPIO 2 - State " + output2State + "</p>");
+            // If the output5State is off, it displays the ON button       
+            if (output2State=="off") {
+              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+            client.println("</body></html>");
+
+          // The HTTP response ends with another blank line
+            client.println();
+
+            //Break out of the while loop
+            break;
+        }else { //if you got a newline, the chear currentLine
+            currentLine = "";
+        }
+      }else if (c=! '\r'){ //if you got anything else but a carriage return character
+        currentLine += c; //add it to the end of the currentLine
+      }
+    }
+  }
+  
+  //Clear the header variable
+  header = "";
+
+  //Close the connection
+  client.stop();
+  Serial.println("client disconected");
+  Serial.println("");
+   
+  }
+}
