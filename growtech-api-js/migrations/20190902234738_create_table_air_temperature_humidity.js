0693b76da2b369fee50060b43d8c80c3c74e71cd
Leonardo Rodrigues Torres <leonardo.torres@LINK-DATA.ASI>
2019-09-03 13:42:45 -0300

    create migrations tables and middlewares

--- growtech-api-js/migrations/20190902234738_create_table_air_temperature_humidity.js
+++ growtech-api-js/migrations/20190902234738_create_table_air_temperature_humidity.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('air_temp_humidity', table => {
+      table.increments('id').primary()
+      table.decimal('temperature', 3, 2)
+      table.decimal('humidity', 3, 2)
+      table.dateTime('created_at')
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('air_temp_humidity')
 };
