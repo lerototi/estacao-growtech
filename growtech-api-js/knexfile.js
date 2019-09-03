d0483680d6f5e1b182c204d8e088d2130a48632b
Lerototi <lerototi@gmail.com>
2019-09-02 23:49:41 -0300

    index.js, db.js and knex

---
+++ growtech-api-js/knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+ 
+    client: 'postgresql',
+    connection: {
+      database: 'growtech',
+      user:     'postgres',
+      password: '1234'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+};
